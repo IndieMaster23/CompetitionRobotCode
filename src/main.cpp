3b37c7876b1018cb834c7701e6f2ed3ebf55485d
IndieMaster23 <oceanicgamer007@gmail.com>
2019-10-10 18:24:08 -0400

    added ablity to control movement, arm, and intake from controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include "main.h"
 
+Motor left (-1);
+Motor right (2);
+
+MotorGroup arm ({-3, -4});
+MotorGroup intake ({-5, 6});
+
+auto chassis = ChassisControllerFactory::create(left, right, AbstractMotor::gearset::green);
+
+Controller controller (ControllerId::master);
+
 void initialize() {}
 
 void disabled() {}
@@ -8,4 +18,30 @@ void competition_initialize() {}
 
 void autonomous() {}
 
-void opcontrol() {}
+void opcontrol() {
+	while (true) {
+		chassis.tank(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::rightY));
+		
+		if (controller.getDigital(ControllerDigital::L1)) {
+			arm.moveVoltage(12000);
+		}
+		else if (controller.getDigital(ControllerDigital::R1)) {
+			arm.moveVoltage(-12000);
+		}
+		else {
+			arm.moveVoltage(0);
+		}
+
+		if (controller.getDigital(ControllerDigital::L2)) {
+			intake.moveVoltage(12000);
+		}
+		else if (controller.getDigital(ControllerDigital::R2)) {
+			intake.moveVoltage(-12000);
+		}
+		else {
+			intake.moveVoltage(0);
+		}
+		
+		pros::Task::delay(20);
+	}
+}
